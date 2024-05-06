6669b6d9d80d2185411a2e9e8f4e4c0203f77f41
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2024-05-06 09:38:25 +0200

    update

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Game extends Board implements ActionListener {
+    static int circleWins = 0;
+    static int crossWins = 0;
     private boolean xTurn = true;
 
     public Game() {
         new Scoreboard();
+    }
+
+    public static int getCircleWins() {
+        return circleWins;
+    }
+
+    public static int getCrossWins() {
+        return crossWins;
     }
 
     @Override
@@ -28,6 +38,13 @@ public class Game extends Board implements ActionListener {
         for (int i = 0; i < 9; i += 3) {
             if (buttons[i].getText().equals(buttons[i + 1].getText()) && buttons[i].getText().equals(buttons[i + 2].getText()) && !buttons[i].isEnabled()) {
                 JOptionPane.showMessageDialog(frame, buttons[i].getText() + " Wins!");
+                if (buttons[i].getText().equals("X")) {
+                    crossWins++;
+                    System.out.println(crossWins);
+                } else if (buttons[i].getText().equals("O")) {
+                    circleWins++;
+                    System.out.println(circleWins);
+                }
                 resetGame();
                 return;
             }
@@ -36,6 +53,11 @@ public class Game extends Board implements ActionListener {
         for (int i = 0; i < 3; i++) {
             if (buttons[i].getText().equals(buttons[i + 3].getText()) && buttons[i].getText().equals(buttons[i + 6].getText()) && !buttons[i].isEnabled()) {
                 JOptionPane.showMessageDialog(frame, buttons[i].getText() + " Wins!");
+                if (buttons[i].getText().equals("X")) {
+                    crossWins++;
+                } else if (buttons[i].getText().equals("O")) {
+                    circleWins++;
+                }
                 resetGame();
                 return;
             }
@@ -43,11 +65,21 @@ public class Game extends Board implements ActionListener {
         //Check Diagonals
         if (buttons[0].getText().equals(buttons[4].getText()) && buttons[0].getText().equals(buttons[8].getText()) && !buttons[0].isEnabled()) {
             JOptionPane.showMessageDialog(frame, buttons[0].getText() + " Wins!");
+            if (buttons[0].getText().equals("X")) {
+                crossWins++;
+            } else if (buttons[0].getText().equals("O")) {
+                circleWins++;
+            }
             resetGame();
             return;
         }
         if (buttons[2].getText().equals(buttons[4].getText()) && buttons[2].getText().equals(buttons[6].getText()) && !buttons[2].isEnabled()) {
             JOptionPane.showMessageDialog(frame, buttons[2].getText() + " Wins!");
+            if (buttons[2].getText().equals("X")) {
+                crossWins++;
+            } else if (buttons[2].getText().equals("O")) {
+                circleWins++;
+            }
             resetGame();
             return;
         }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,12 +1,14 @@
 import java.io.*;
 
 public class Scoreboard {
+
     public Scoreboard() {
         try {
             File score = new File("scoreboard.txt");
-            FileWriter sWriter = new FileWriter(score);
-            sWriter.write("Cross: " + System.lineSeparator());
-            sWriter.write("Circle: ");
+            try (FileWriter sWriter = new FileWriter("scoreboard.txt")) {
+                sWriter.write("Cross: " + Game.getCrossWins() + System.lineSeparator());
+                sWriter.write("Circle: " + Game.getCircleWins());
+            }
             if (score.createNewFile()) {
                 System.out.println(score.getName() + " created");
             } else {
