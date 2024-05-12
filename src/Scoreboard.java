0abfce5b10966ee3410f74dc9efbcec171dc22b0
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2024-05-12 21:30:23 +0200

    update

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,18 +5,19 @@ public class Scoreboard {
     public Scoreboard() {
         try {
             File score = new File("scoreboard.txt");
-            try (FileWriter sWriter = new FileWriter("scoreboard.txt")) {
-                sWriter.write("Cross: " + Game.getCrossWins() + System.lineSeparator());
-                sWriter.write("Circle: " + Game.getCircleWins());
-            }
+            FileWriter sWriter = new FileWriter("scoreboard.txt");
+            FileReader sReader = new FileReader("scoreboard.txt");
+
             if (score.createNewFile()) {
                 System.out.println(score.getName() + " created");
+                sWriter.write("Cross: " + Game.getCrossWins() + System.lineSeparator());
+                sWriter.write("Circle: " + Game.getCircleWins());
+
             } else {
                 System.out.println("Scoreboard already exists");
             }
-        } catch (IOException exception) {
+        } catch (IOException e) {
             System.out.println("Unexpected error occurred");
-            exception.printStackTrace();
         }
     }
 }
